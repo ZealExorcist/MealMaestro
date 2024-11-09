11019fa861d0ce53aee03ff94f19a1b7e6c4ebf4
ZealExorcist <141896261+ZealExorcist@users.noreply.github.com>
2024-11-10 04:24:20 +0530

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -9,7 +9,7 @@ st.title("Recipe Finder")
 with st.spinner():
     st.write("Enter the food item you want to search for:")
     input = st.text_input("Enter food item")
-    prompt = st.session_state.prompt + "recipe for " + input
+    st.session_state.prompt = st.session_state.prompt + "recipe for " + input
     if st.button("Search"):
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
         model = genai.GenerativeModel("gemini-1.5-flash")
